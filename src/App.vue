5b0f1ec0616c76a6d0b2b1bcc03734bc1a5f86e0
Toxirrrr <mjuraeva74@gmail.com>
2024-10-20 11:39:49 +0500

    Toxirrrr

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import HeaderComp from './components/Header/HeaderComp.vue'
+import FooterComp from './components/Footer/FooterComp.vue'
 </script>
 
 <template>
   <HeaderComp />
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
+  <FooterComp />
 </template>
