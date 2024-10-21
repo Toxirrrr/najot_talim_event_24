06ff403a602a3a7bf36b86877e3bed27c007e8c2
Toxirrrr <mjuraeva74@gmail.com>
2024-10-22 03:48:18 +0500

    Toxirrrr

--- js/login.js
+++ js/login.js
@@ -1,46 +1,44 @@
 let elLoginForm = document.querySelector('.login__form')
 let elHeaderTitle = document.querySelector('.header__mid-login')
 let elHeaderProfileLink = document.querySelector('.header__login')
-let elUserInput = document.querySelector('#username').value
-let elPassInput = document.querySelector('#password').value
+let elUserInput = document.querySelector('#username')
+let elPassInput = document.querySelector('#password')
 let api_new = 'https://6714b258690bf212c762246c.mockapi.io/'
 let user = localStorage.getItem('user')
 let newUser = JSON.parse(user)
 
-if (newUser){
+if (newUser) {
     elHeaderTitle.textContent = newUser.username
     elHeaderProfileLink.href = '/profile.html'
-}else {
+} else {
     localStorage.removeItem('user')
 }
 
-if(location.pathname == '/profile.html' && !newUser) {
-    location.href = '/'
+if (location.pathname == '/profile.html' && !newUser) {
+    location.href = '/login'
+} else if (location.pathname == '/login.html' && newUser) {
+    location.href = '/profile'
 }
 
-if(location.pathname == '/login.html' && newUser) {
-    location.href = '/'
-}
+elLoginForm?.addEventListener('submit', async function signIn(evt) {
+    evt.preventDefault();
 
-    elLoginForm?.addEventListener('submit', async function signIn(evt) {
-        evt.preventDefault();
-
-        try {
-            let res = await fetch(`${api_new}login`, {
-                headers: { 'Content-Type': 'application/json' },
-                method: 'POST',
-                body: JSON.stringify({
-                    username: elUserInput || 'Duo Vs Trio',
-                    password: elPassInput || 'Duo Vs Trio'
-                })
+    try {
+        let res = await fetch(`${api_new}login`, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            body: JSON.stringify({
+                username: elUserInput.value || 'Duo Vs Trio',
+                password: elPassInput.value || 'Duo Vs Trio'
             })
-            res = await res.json()
+        })
+        res = await res.json()
 
-            if (res.createdAt) {
-                localStorage.setItem('user', JSON.stringify(res))
-                location.href = '/'
-            }
-        } catch (error) {
-            console.log('login err', error);
+        if (res.createdAt) {
+            localStorage.setItem('user', JSON.stringify(res))
+            location.href = '/'
         }
-    })
+    } catch (error) {
+        console.log('login err', error);
+    }
+})
